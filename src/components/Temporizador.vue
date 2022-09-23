97405387618584dcfed49fb23f7f4c205fd38d94
celiorscosta <celiorscosta@hotmail.com>
2022-09-23 09:44:59 -0300

    Refatorando o botão

--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,21 +1,11 @@
 <template>
-    <div class="is-flex is-align-items-center is-justify-content-space-between">
-        <Cronometro :tempoEmSegundos="tempoEmSegundos" />
-        <button class="button" @click="iniciar" :disabled="cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-        </button>
-        <button class="button" @click="finalizar" :disabled="!cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-        </button>
-    </div>
-</template>
-
+    <section class="is-flex is-align-items-center is-justify-content-space-between">
+      <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
+      <Botao @clicado="iniciar" icone="fas fa-play" texto="play" :desabilitado="cronometroRodando" />
+      <Botao @clicado="finalizar" icone="fas fa-stop" texto="stop" :desabilitado="!cronometroRodando" />
+    </section>
+  </template>
+  
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Cronometro from './Cronometro.vue';
