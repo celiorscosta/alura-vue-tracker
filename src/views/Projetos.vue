5cddcc9861b7ab88fbbb295771d76f65bb1e9435
Célio Costa <celiorscosta@hotmail.com>
2022-09-26 16:08:04 -0300

    Tudo ok. Adicionando projetos.

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -34,8 +34,8 @@
 </template>
 
 <script lang="ts">
-import IProjeto from "@/interfaces/IProjeto";
-import { defineComponent } from "vue";
+import { useStore } from "@/store";
+import { computed, defineComponent } from "vue";
 
 export default defineComponent({
   // eslint-disable-next-line
@@ -43,18 +43,20 @@ export default defineComponent({
   data() {
     return {
       nomeDoProjeto: "",
-      projetos: [] as IProjeto[],
     };
   },
   methods: {
     salvar() {
-      const projeto: IProjeto = {
-        nome: this.nomeDoProjeto,
-        id: new Date().toISOString(),
-      };
-      this.projetos.push(projeto);
+      this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto);
       this.nomeDoProjeto = "";
     },
+  },
+  setup() {
+    const store = useStore();
+    return {
+      store,
+      projetos: computed(() => store.state.projetos),
+    };
   },
 });
 </script>
