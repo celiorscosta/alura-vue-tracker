6c203cc84bddfba303e4d68eaf951ac32f2dfab4
Célio Costa <celiorscosta@hotmail.com>
2022-09-26 12:22:00 -0300

    Refatoração para adicionar rotas - curso 2

---
+++ src/views/Tarefas.vue
@@ -0,0 +1,41 @@
+<template>
+  <Formulario @aoSalvarTarefa="salvarTarefa" />
+  <div class="lista">
+    <Box v-if="listaEstaVazia"> Você não está muito produtivo hoje :( </Box>
+    <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Formulario from "../components/FormularioTarefa.vue";
+import Tarefa from "../components/Tarefa.vue";
+import ITarefa from "../interfaces/ITarefas";
+import Box from "../components/Box.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Formulario,
+    Tarefa,
+    Box,
+  },
+  data() {
+    return {
+      tarefas: [] as ITarefa[],
+      modoEscuroAtivo: false,
+    };
+  },
+  computed: {
+    listaEstaVazia(): boolean {
+      return this.tarefas.length === 0;
+    },
+  },
+  methods: {
+    salvarTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa);
+    }
+  },
+});
+</script>
+
