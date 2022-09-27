9a280c75666a5cb6528d7b7447d70787284675b5
Célio Costa <celiorscosta@hotmail.com>
2022-09-27 13:09:38 -0300

    Aula 05 - Mixin

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -19,8 +19,9 @@
 
 <script lang="ts">
 import { TipoDeNotificacao } from "@/interfaces/INotificacao";
+import { notificacaoMixin } from "@/mixins/notificar";
 import { useStore } from "@/store";
-import { ALTERA_PROJETO, ADICIONA_PROJETO, NOTIFICAR } from "@/store/tipo-mutacoes";
+import { ALTERA_PROJETO, ADICIONA_PROJETO } from "@/store/tipo-mutacoes";
 import { defineComponent } from "vue";
 
 export default defineComponent({
@@ -31,6 +32,7 @@ export default defineComponent({
       type: String,
     },
   },
+  mixins: [notificacaoMixin],
   mounted() {
     if (this.id) {
       const projeto = this.store.state.projetos.find(
@@ -55,11 +57,11 @@ export default defineComponent({
         this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto);
       }
       this.nomeDoProjeto = "";
-      this.store.commit(NOTIFICAR, {
-        titulo: 'Novo projeto foi salvo',
-        texto: 'Prontinho ;) seu projeto já está disponível.',
-        tipo: TipoDeNotificacao.SUCESSO
-      })
+      this.notificar(
+        TipoDeNotificacao.SUCESSO,
+        "Excelente",
+        "O projeto foi cadastrado com sucesso."
+      );
       this.$router.push("/projetos");
     },
   },
