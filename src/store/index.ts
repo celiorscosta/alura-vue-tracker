3b7319aac0ca7c2424efbed8c113b145194b05b3
Célio Costa <celiorscosta@hotmail.com>
2022-09-27 11:09:35 -0300

    Aula 03

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import IProjeto from "@/interfaces/IProjeto";
 import { InjectionKey } from "vue";
 import { createStore, Store, useStore as vuexUsestore } from "vuex";
+import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO } from "./tipo-mutacoes";
 
 interface Estado {
     projetos: IProjeto[]
@@ -13,21 +14,24 @@ export const store = createStore<Estado>({
         projetos: []
     },
     mutations: {
-        'ADICIONA_PROJETO'(state, nomeDoProjeto: string) {
+        [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
             const projeto = {
                 id: new Date().toISOString(),
                 nome: nomeDoProjeto
             } as IProjeto;
             state.projetos.push(projeto);
         },
-        'ALTERA_PROJETO'(state, projeto: IProjeto){
-            const index = state.projetos.findIndex(proj=> proj.id == projeto.id);
+        [ALTERA_PROJETO](state, projeto: IProjeto) {
+            const index = state.projetos.findIndex(proj => proj.id == projeto.id);
             state.projetos[index] = projeto;
+        },
+        [EXCLUIR_PROJETO](state, id: string) {
+            state.projetos = state.projetos.filter(proj => proj.id != id);
         }
 
     }
 });
 
-export function useStore(): Store<Estado>{
-return vuexUsestore(key);
+export function useStore(): Store<Estado> {
+    return vuexUsestore(key);
 }
--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -19,6 +19,7 @@
 
 <script lang="ts">
 import { useStore } from "@/store";
+import { ALTERA_PROJETO, ADICIONA_PROJETO } from "@/store/tipo-mutacoes";
 import { defineComponent } from "vue";
 
 export default defineComponent({
@@ -45,12 +46,12 @@ export default defineComponent({
   methods: {
     salvar() {
       if (this.id) {
-        this.store.commit("ALTERA_PROJETO", {
+        this.store.commit(ALTERA_PROJETO, {
           id: this.id,
           nome: this.nomeDoProjeto,
         });
       } else {
-        this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto);
+        this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto);
       }
       this.nomeDoProjeto = "";
       this.$router.push("/projetos");
